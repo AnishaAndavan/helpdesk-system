800e28db7bab231ce78a58b22315d05019bf2cbc
AnishaAndavan <anishaandavan503@gmail.com>
2025-11-12 20:23:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 dotenv.config({ path: path.join(__dirname, "../.env") });
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_HOST:", process.env.MYSQLHOST);
+console.log("DB_USER:", process.env.MYSQLUSER);
+console.log("DB_NAME:", process.env.MYSQLDATABASE);
 
 const app = express();
 
@@ -31,5 +31,5 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "login.html"));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.MYSQLPORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
